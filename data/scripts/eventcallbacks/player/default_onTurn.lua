5802b9bc7204944fec769d8e9d1b70db6eabdc4c
TheMaoci <themaoci@gmail.com>
2022-09-18 11:50:20 +0200

    update map / talkactions on turn and gain tries

--- data/scripts/eventcallbacks/player/default_onTurn.lua
+++ data/scripts/eventcallbacks/player/default_onTurn.lua
@@ -1,10 +1,9 @@
 local ec = EventCallback
 
 ec.onTurn = function(self, direction)
-    if self:getGroup():getAccess() and self:getDirection() == direction then
+    if self:getGroup():getAccess() and self:getDirection() == direction and self:isInGhostMode() then
         local nextPosition = self:getPosition()
         nextPosition:getNextPosition(direction)
-
         self:teleportTo(nextPosition, true)
     end
 	return true
