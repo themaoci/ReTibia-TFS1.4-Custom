87822c161d14965f7312a10d3449f84323587458
TheMaoci <themaoci@gmail.com>
2022-09-19 14:51:22 +0200

    script to allow going past the ghoul wall in katana quest room

---
+++ data/actions/scripts/rookgaard/ghoul_wall_passing.lua
@@ -0,0 +1,23 @@
+local gwp_wallpos = {x=455, y=321, z=11}
+local gwp_wallRightpos = {x=456, y=321, z=11}
+local gwp_wallLeftpos = {x=454, y=321, z=11}
+function onUse(cid, item, frompos, item2, topos)
+ 
+  if getPlayerStorageValue(cid, 7019) > 0 and getPlayerStorageValue(cid, 7038) > 0 then 
+    if frompos.x == gwp_wallRightpos.x and frompos.y == gwp_wallRightpos.y then
+      frompos.x = gwp_wallLeftpos.x
+      frompos.y = gwp_wallLeftpos.y
+      player:teleportTo(frompos, false)
+      return 1
+    end
+    if frompos.x == gwp_wallLeftpos.x and frompos.y == gwp_wallLeftpos.y then
+      frompos.x = gwp_wallRightpos.x
+      frompos.y = gwp_wallRightpos.y
+      player:teleportTo(frompos, false)
+      return 1
+    end
+  end
+  doPlayerSendCancel(cid, "Seems like this wall is different from the others here...")
+  return 1
+end
+  
