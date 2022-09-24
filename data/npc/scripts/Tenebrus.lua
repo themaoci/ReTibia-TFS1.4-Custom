f747859d62bc390b534361d0cb9a7fe3f03126a3
TheMaoci <themaoci@gmail.com>
2022-09-24 18:11:37 +0200

    Update Tenebrus.lua

--- data/npc/scripts/Tenebrus.lua
+++ data/npc/scripts/Tenebrus.lua
@@ -97,7 +97,7 @@ function creatureSayCallback(cid, type, msg)
         local playerCLevel = tonumber(math.max(unpack({getPlayerSkill(cid, 0), getPlayerSkill(cid, 1), getPlayerSkill(cid, 2), getPlayerSkill(cid, 3)})))
 
         for var, item in pairs(GameConfig.BuySpellList) do
-            if not getPlayerLearnedInstantSpell(item.words) then
+            if not getPlayerLearnedInstantSpell(cid, item.words) then
                 local additionalName = ""
                 if item.group == "runeMaking" then
                     additionalName = " Making"
@@ -130,7 +130,7 @@ function creatureSayCallback(cid, type, msg)
             
             local canlearnspellexists = canLearnSpell[item]
             if canlearnspellexists ~= nil then
-                if not getPlayerLearnedInstantSpell(canlearnspellexists.words) then
+                if not getPlayerLearnedInstantSpell(cid, canlearnspellexists.words) then
                     doPlayerRemoveMoney(cid, canlearnspellexists.price)
                     doPlayerLearnInstantSpell(cid, canlearnspellexists.words)
                     npcHandler:say("Thank you, I received your payment of " .. canlearnspellexists.price .. "gp. So you have learned spell " .. canlearnspellexists.name .. " with incantation of '" .. canlearnspellexists.words .. "'. Now you can use it freely untill you have enough of mana.", cid)
@@ -140,10 +140,10 @@ function creatureSayCallback(cid, type, msg)
             else
                 local missingRequirements = ""
 
-                local item_level = tonumber(item.level or 0)
-                local item_mlevel = tonumber(item.mlevel or 0)
-                local item_clevel = tonumber(item.clevel or 0)
-                local item_dlevel = tonumber(item.dlevel or 0)
+                local item_level = tonumber(canlearnspellexists.level or 0)
+                local item_mlevel = tonumber(canlearnspellexists.mlevel or 0)
+                local item_clevel = tonumber(canlearnspellexists.clevel or 0)
+                local item_dlevel = tonumber(canlearnspellexists.dlevel or 0)
 
                 if item_level <= playerLevel then
                     missingRequirements = missingRequirements .. "Level required is " .. item_level .. " but you have " .. playerLevel
