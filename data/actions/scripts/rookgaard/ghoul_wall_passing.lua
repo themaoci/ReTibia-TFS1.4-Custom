613f7fb0df425acc9f841d2b94274878d7e14f12
TheMaoci <themaoci@gmail.com>
2022-09-24 18:49:56 +0200

    Update ghoul_wall_passing.lua

--- data/actions/scripts/rookgaard/ghoul_wall_passing.lua
+++ data/actions/scripts/rookgaard/ghoul_wall_passing.lua
@@ -4,6 +4,8 @@ local gwp_wallLeftpos = {x=454, y=321, z=11}
 function onUse(cid, item, frompos, item2, topos)
  
   if getPlayerStorageValue(cid, 7019) == 1 and getPlayerStorageValue(cid, 7038) == 1 then 
+    Discord_Debug(frompos.x .. " == " .. gwp_wallRightpos.x .. " " ..  frompos.y .. " == " .. gwp_wallRightpos.y)
+    Discord_Debug(frompos.x .. " == " .. gwp_wallLeftpos.x .. " " ..  frompos.y .. " == " .. gwp_wallLeftpos.y)
     if frompos.x == gwp_wallRightpos.x and frompos.y == gwp_wallRightpos.y then
       frompos.x = gwp_wallLeftpos.x
       frompos.y = gwp_wallLeftpos.y
@@ -16,9 +18,8 @@ function onUse(cid, item, frompos, item2, topos)
       player:teleportTo(frompos, false)
       return 1
     end
+    return 1
   end
-  print(getPlayerStorageValue(cid, 7019))
-  print(getPlayerStorageValue(cid, 7038))
   doPlayerSendCancel(cid, "Seems like this wall is different from the others here...")
   return 1
 end
