3e5b889c552c97bb6b9e07c32df91d41e9e12835
TheMaoci <themaoci@gmail.com>
2022-09-24 12:11:03 +0200

    Update Tenebrus.lua

--- data/npc/scripts/Tenebrus.lua
+++ data/npc/scripts/Tenebrus.lua
@@ -89,7 +89,7 @@ function creatureSayCallback(cid, type, msg)
             local skill_distance = item.distlevel or 0
 
             shopWindow[item.id] = {Level = level, MLevel = magiclevel, CLevel = skill_closeCombat, DLevel = skill_distance, Price = item.price, Words = item.words, SpellName = item.name}
-            table.insert(spells, {id=1950, buy = item.price, name = item.words, spell = item.name, vocations = {0,1,2,3,4,5,6,7,8}, level = level}
+            table.insert(spells, {id=1950, buy = item.price, name = item.words, spell = item.name, vocations = {0,1,2,3,4,5,6,7,8}, level = level})
         end
 
 
