02dd4a98dbc250c04a7b356b1233ff455061e95b
TheMaoci <themaoci@gmail.com>
2022-09-25 18:09:42 +0200

    my mistake for copy pasting...

--- src/luascript.h
+++ src/luascript.h
@@ -589,10 +589,10 @@ class LuaScriptInterface
 		static int luaGameSetAccountStorageValue(lua_State* L);
 		static int luaGameSaveAccountStorageValues(lua_State* L);
 
-		static int LuaScriptInterface::luaGameGetAllSpells(lua_State* L);
-		static int LuaScriptInterface::luaGameGetAllRunes(lua_State* L);
-		static int LuaScriptInterface::luaGameGetAllOutfits(lua_State* L);
-		static int LuaScriptInterface::luaGameGetAllMounts(lua_State* L);
+		static int luaGameGetAllSpells(lua_State* L);
+		static int luaGameGetAllRunes(lua_State* L);
+		static int luaGameGetAllOutfits(lua_State* L);
+		static int luaGameGetAllMounts(lua_State* L);
 		
 		// Variant
 		static int luaVariantCreate(lua_State* L);
