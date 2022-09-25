c11c53bb29411f336d52d44eed7313e07e6c4426
TheMaoci <themaoci@gmail.com>
2022-09-25 21:59:46 +0200

    Update logout.lua

--- data/creaturescripts/scripts/logout.lua
+++ data/creaturescripts/scripts/logout.lua
@@ -3,6 +3,6 @@ function onLogout(player)
 	if nextUseStaminaTime[playerId] then
 		nextUseStaminaTime[playerId] = nil
 	end
-  db.query("UPDATE `players` SET `isonline` = '0' WHERE `id` = " .. player:getGuid())
+  	db.query("UPDATE `players` SET `isonline` = '0' WHERE `id` = " .. player:getGuid())
 	return true
 end
