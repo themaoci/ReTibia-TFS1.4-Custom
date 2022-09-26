8be89a249ef11da4517ec7d40f0b11c4f16e8716
TheMaoci <themaoci@gmail.com>
2022-09-26 17:24:30 +0200

    Update mass_healing.lua

--- data/spells/scripts/healing/mass_healing.lua
+++ data/spells/scripts/healing/mass_healing.lua
@@ -8,17 +8,17 @@ function onCastSpell(creature, variant)
 	print(type(creature))
 	print(type(variant))
 	print(variant)
-if creature == nil then 
-	print(creature)
-	print(creature:getName())
-	--local min = 104.8
-	--local max = 134.8
-	return true 
-end
-local creatureLevel = creature:getLevel() or 1
-local creatureMLevel = creature:getMagicLevel() or 1
-  local min = (creatureLevel / 5) + (creatureMLevel * 4.6) + 100
-  local max = (creatureLevel / 5) + (creatureMLevel * 9.6) + 125
+	if creature == nil then 
+		print(creature)
+		print(creature:getName())
+		--local min = 104.8
+		--local max = 134.8
+		return true 
+	end
+	local creatureLevel = creature:getLevel() or 1
+	local creatureMLevel = creature:getMagicLevel() or 1
+	local min = (creatureLevel / 5) + (creatureMLevel * 4.6) + 100
+	local max = (creatureLevel / 5) + (creatureMLevel * 9.6) + 125
 	for _, target in ipairs(combat:getTargets(creature, variant)) do
 		local master = target:getMaster()
 		if target:isPlayer() or master and master:isPlayer() then
