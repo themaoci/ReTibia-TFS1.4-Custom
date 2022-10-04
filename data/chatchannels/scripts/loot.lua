d4d353efb8f31508a0d18d47a1acc63b89cc17f2
TheMaoci <themaoci@gmail.com>
2022-10-04 17:39:55 +0200

    Update loot.lua

--- data/chatchannels/scripts/loot.lua
+++ data/chatchannels/scripts/loot.lua
@@ -1,12 +1,3 @@
 function onSpeak(player, type, message)
     return false
 end
-function onJoin(player)
-    --player:setStorageValue(STORAGEVALUE_LOOT, 1)
-    return true
-end
-
-function onLeave(player)
-    --player:setStorageValue(STORAGEVALUE_LOOT, 0)
-    return true
-end
