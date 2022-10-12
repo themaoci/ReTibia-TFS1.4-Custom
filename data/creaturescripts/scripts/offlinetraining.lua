5382ba814a7e3c7c97013558330bdd3d232b5fba
TheMaoci <themaoci@gmail.com>
2022-10-12 20:34:42 +0200

    Update offlinetraining.lua

--- data/creaturescripts/scripts/offlinetraining.lua
+++ data/creaturescripts/scripts/offlinetraining.lua
@@ -9,7 +9,7 @@ function onLogin(player)
 
 	player:setOfflineTrainingSkill(-1)
 
-	if offlineTime < 600 then
+	if offlineTime < 10 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must be logged out for more than 10 minutes to start offline training.")
 		return true
 	end
